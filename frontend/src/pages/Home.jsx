2126536a4b0606b1eca0f3ceb3ca41885e0ab37d
Pavan-Rajana <pkcoder.res@gmail.com>
2024-11-08 17:41:26 +0530

    Home

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,9 +1,15 @@
+import Banner from "../components/Banner"
 import Header from "../components/Header"
+import SpecialtyMenu from "../components/SpecialtyMenu"
+import TopDoctors from "../components/TopDoctors"
 
 const Home = () => {
     return (
         <div>
             <Header />
+            <SpecialtyMenu />
+            <TopDoctors />
+            <Banner />
         </div>
     )
 }
