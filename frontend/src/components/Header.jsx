43c00bf0ba196cb4a681fbc8c91251415c518dd8
Pavan-Rajana <pkcoder.res@gmail.com>
2024-11-08 17:40:52 +0530

    Header

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -12,7 +12,7 @@ const Header = () => {
                     <img className="w-28" src={f_assets.group_profiles} alt="" />
                     <p>Lorem ipsum, dolor sit amet consectetur <br className="hidden sm:block" /> adipisicing elit.</p>
                 </div>
-                <a className="flex gap-2 items-center text-gray-600 m-auto text-sm md:m-0 bg-white px-6 py-3 rounded-full hover:scale-105 transition-all duration-300" href="">Book Appointment <img className="w-3" src={f_assets.arrow_icon} alt="" /></a>
+                <a className="flex gap-2 items-center text-gray-600 m-auto text-sm md:m-0 bg-white px-6 py-3 rounded-full hover:scale-105 transition-all duration-300" href="#specialty">Book Appointment <img className="w-3" src={f_assets.arrow_icon} alt="" /></a>
             </div>
             {/* right side */}
             <div className="md:w-1/2 relative">
